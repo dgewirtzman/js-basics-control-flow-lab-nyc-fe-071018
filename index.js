4f8d2791d52eebf4bca73b7f78e072e7483bcdc7
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-09 23:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 let rideLength = 199;
 let greeting;
 
-function scuberGreetingForFeet() {
+function scuberGreetingForFeet(rideLength) {
   if (rideLength < 400) {
     greeting = "This one is on me!";
   }
