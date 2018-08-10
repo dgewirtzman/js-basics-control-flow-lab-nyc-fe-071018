20faabcdd97d231b9f8116c5771d8fdd9619929d
David Gewirtzman <dgewirtzman@playbill.com>
2018-08-10 00:25:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function scuberGreetingForFeet(someValue) {
 }
 
 function ternaryCheckCity(someValue) {
-  return (someValue = 'NYC' ? 'Ok, sounds good.' : 'No go.')
+  return (someValue = 'NYC' ? 'Ok, sounds good.' : 'No go.');
 }
